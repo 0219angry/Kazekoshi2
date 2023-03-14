8842acb92152d59933bebe2c19e6907731f9185e
0219Angry <angry.yuma.0219@gmail.com>
2023-03-14 09:18:58 +0000

    feat: コマンド”r"の名称を"c"に変更

--- Kazekoshi.py
+++ Kazekoshi.py
@@ -6,14 +6,13 @@ import configparser
 import discord
 from discord.ext import commands
 
-# bot token
-TOKEN = ""
+
 
 # create dir
 if not os.path.exists("log"):
     os.makedirs("log")
 
-if not os.path.exits("temp"):
+if not os.path.exists("temp"):
     os.makedirs("temp")
 
 # log
@@ -45,27 +44,47 @@ except:
     sys.exit()
 
 
-intents = discord.Intents.default() 
-client = discord.Client(command_prefix=COMMAND_PREFIX, intents=intents)
+intents = discord.Intents.all() 
+client = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents, help_command=None)
 
 @client.event
 async def on_ready():
     logger.info("Kazekoshi v2.0 on ready")
 
 @client.command()
-async def help(ctx):
+async def help(ctx: commands.Context):
     logger.info("requested command : help")
 
 
 @client.command()
-async def r(ctx):
-    logger.info("requested command : read")
+async def c(ctx: commands.Context):
+    logger.info("requested command : connect")
 
     if ctx.author.voice is None:
         await ctx.channel.send("あなたはボイスチャンネルに接続していません")
-        logger.info(f"{ctx.author} is not connect voicechannel")
+        logger.info(f"{ctx.author} is not connected voice channel")
         return
     
+    global connected_channel
+
+    async def connect(ctx: commands.Context):
+        con_embed = discord.Embed(title="読み上げ開始")
+        con_embed.add_field(name="読み上げテキストチャンネル", value=f"{ctx.channel.name}", inline="false")
+        con_embed.add_field(name="読み上げボイスチャンネル", value=f"{ctx.author.voice.channel.name}",inline="false")
+        await ctx.channel.send(embed=con_embed)
+
+        await ctx.author.voice.channel.connect()
+
+        connected_channel = ctx.author.voice.channel
+
+        logger.info(f"Start reading {ctx.channel.name} at {ctx.author.voice.channel.name}")
+    
+    if ctx.guild.voice_client is not None:
+        await ctx.guild.voice_client.disconnect()
+    
+    await connect(ctx)
+    return
+
 
 
 client.run(DISCORD_TOKEN)
